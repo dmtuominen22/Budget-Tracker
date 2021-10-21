e49a6801bcde303df0adc17d8c7bbf90f7ab781d
Denise <dmtuominen@gmail.com>
2021-10-20 19:33:04 -0600

    fix cod on service

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,44 +20,39 @@ const FILES_TO_CACHE = [
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
+        caches.open(CACHE_NAME).then(cache => {
             console.log('installing cache : ' + CACHE_NAME);
             return cache.addAll(FILES_TO_CACHE)
         })
+        .then(self.skipWaiting())
     )
-});
+})
 
 self.addEventListener('activate', function (e) {
     e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeeplist.push(CACHE_NAME);
-
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheKeeplist.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i] );
-                    return caches.delete(keyList[i]);
+        caches.keys()
+        .then((keyList) => {  
+            return Promise.all(keyList.map((key) => {
+                if (key !== PRECACHE) {
+                    console.log('[ServiceWorker] Removing old cache', key)
+                    return caches.delete(key)
                 }
-            }));
+            }))
         })
+        .then(() => self.clientInformation.claim() )
     )
 });
 
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url);
     e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url);
-                return request
-            } else {       // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url);
-                return fetch(e.request)
-            }
-
+        fetch(e.request)
+        .catch(() => {
+            return cashes.open(version_01)
+            .then((cache) => {
+                return cache.match(e.request)
+            })
         })
     )
-});
+})
 
